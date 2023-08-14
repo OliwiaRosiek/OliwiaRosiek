6ef5f425aa48afa06b5f3729533d57c1d32dac99
OliwiaRosiek <106026284+OliwiaRosiek@users.noreply.github.com>
2023-08-14 23:39:50 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   const myBirthDate = Date.parse(DATE_OF_BIRTH);
   const now = new Date();
   const difference = (now.getTime() - myBirthDate.getTime()) / (1000 * 60 * 60 * 24);
-  const differenceInYears = Math.round(difference/365.25));
+  const differenceInYears = Math.round(difference/365.25);
   document.getElementById("my-age").innerHTML = differenceInYears;
   
 });
